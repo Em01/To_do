75202b038ecc836a3bf37b163d6c3c4087715d4a
Em01 <emma_williams1@hotmail.co.uk>
2015-01-12 12:21:49 +0000

    routes for complete item

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do 
+    	patch :complete
+    	member do 
+    	end
+    end
   end
 
   root "todo_lists#index"
